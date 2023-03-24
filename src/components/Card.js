ce0a3065514033f7178a93668264e1b2aeaf965c
Mike Teuten <mike.teuten@gmail.com>
2023-03-24 18:28:07 +0000

    final

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 function Card(props) {
   return (
     // Flex code
-    <div className="flex flex-col shadow-md w-fullm-6 overflow-hidden sm:w-5">
+    <div className="flex flex-col shadow-md w-64 overflow-hidden p-1">
       <div className="max-w-sm rounded overflow-hidden shadow-lg bg-gray-400">
         <div className="px-6 py-4">
           <div className="font-bold text-xl mb-2">
